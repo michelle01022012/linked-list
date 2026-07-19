515d1ead3272eb10640be41a19937c37f01dd8ef
Michelle Anderson <manderson0102@yahoo.com>
2026-07-19 17:16:56 -0400

    updated for linked list cycles

--- questions/linkedLists/linkedListCycle2/index.js
+++ questions/linkedLists/linkedListCycle2/index.js
@@ -11,8 +11,19 @@
  */
 
  const listCycle = (head) => {
+    let fast = head
+    let slow = head
 
+    while (fast && fast.next){
+        slow = slow.next
+        fast = fast.next.next
 
+        if (slow === fast){
+            return true
+        }
+    }
+
+        return false
 }
 
 
