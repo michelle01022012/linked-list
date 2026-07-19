c66171f8d0ccaf72423adf2d967f3b0d96e24088
Michelle Anderson <manderson0102@yahoo.com>
2026-07-19 17:20:10 -0400

    reversed list code

--- questions/linkedLists/reverseLinkedList/index.js
+++ questions/linkedLists/reverseLinkedList/index.js
@@ -10,6 +10,17 @@
  */
 
  const reverse = (head) => {
+    let curr = head
+    let prev = null
+
+    while (curr){
+        const next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+
+    return prev
   
 }
 
