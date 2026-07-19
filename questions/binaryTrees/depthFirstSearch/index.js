cb72c5a3c8ed5c90ac672c7ca5f813c57ac4ab15
Michelle Anderson <manderson0102@yahoo.com>
2026-07-19 18:37:00 -0400

    finished depth first search code

--- questions/binaryTrees/depthFirstSearch/index.js
+++ questions/binaryTrees/depthFirstSearch/index.js
@@ -6,8 +6,18 @@
  */
 
  const depthFirstSearch = (root) => {
+    const result = []
+    const stack = [root]
 
-}
+    while (stack.length > 0){
+        const current = stack.pop()
+        result.push(current.val)
+
+        if (current.right) stack.push(current.right)
+        if (current.left) stack.push(current.left)
+    }
+        return result
+   }
 
 
 module.exports = depthFirstSearch;
