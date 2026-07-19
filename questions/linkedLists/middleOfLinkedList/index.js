c557b312d6a832cefa4a852a0852c12057bda767
Michelle Anderson <manderson0102@yahoo.com>
2026-07-19 17:11:42 -0400

    updates for middle of linked lists

--- questions/linkedLists/middleOfLinkedList/index.js
+++ questions/linkedLists/middleOfLinkedList/index.js
@@ -10,8 +10,14 @@
  */
 
  const middle = (head) => {
+  let slow = head
+  let fast = head
 
-
+  while(fast&& fast.next){
+    slow = slow.next
+    fast = fast.next.next
+  }
+ return slow
 }
 
 
